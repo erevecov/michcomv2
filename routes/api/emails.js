0e1ad02edaca5d4b626a91eff3a278545e7aa0e9
Eduardo Reveco <eduardorevecovillalobos@gmail.com>
2018-07-30 18:15:45 -0400

    test

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -13,7 +13,7 @@ var config = {
 var transporter = nodemailer.createTransport({
     host: process.env.EMAIL_SERVER,
     port: 587,
-    secure: true, // use TLS
+    secure: false, // use TLS
     auth: {
         user: process.env.EMAIL_USER, // .env para commit
         pass: process.env.EMAIL_PASSWORD // .env para commit
